e7d47c83946e72ecb4275630d5526383ef3bb572
Shu Oomura <ohs6261@gmail.com>
2020-07-25 22:33:03 +0900

    Add prefectures using csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,14 @@
-Prefecture.create!(
-  name: "大阪府",
-)
-Prefecture.first.cities.create!(
-  name: "大阪",
-  romaji: "Osaka",
-  city_id: 1853908,
+require 'csv'
+
+CSV.foreach('db/csv/prefectures.csv') do |row|
+  Prefecture.create!(
+    name: row[1]
+  )
+end
+
+osaka = Prefecture.find_by(name: '大阪府')
+osaka.cities.create!(
+  name: '大阪',
+  romaji: 'Osaka',
+  location_id: 1853908,
 )
