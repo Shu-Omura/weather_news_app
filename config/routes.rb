85da185465a89dfbd4ec35c51e3bf16b105f11d8
Shu Oomura <ohs6261@gmail.com>
2020-07-27 02:44:42 +0900

    Arrange styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'prefecture#index'
   resources :prefectures, only: [:index], shallow: true do
     resources :cities, only: [:index] do
       resources :weather_forecasts, only: [:index]
