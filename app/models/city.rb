be7132d034aa9930b27065b10e85a2d2e1e50bce
Shu Oomura <ohs6261@gmail.com>
2020-07-28 20:14:09 +0900

    Add model spec for cities

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,7 @@
 class City < ApplicationRecord
   belongs_to :prefecture
   has_many :weather_forecasts, dependent: :destroy
+  validates :name, presence: true
+  validates :romaji, presence: true
+  validates :location_id, presence: true
 end
--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe City, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    describe 'factory' do
+      let(:city) { build(:city) }
+      it 'is valid' do
+        expect(city).to be_valid
+      end
+    end
+
+    describe 'about name' do
+      let(:city_2) { build(:city, name: nil) }
+      it 'is invalid without name' do
+        expect(city_2).not_to be_valid
+      end
+    end
+
+    describe 'about romaji' do
+      let(:city_3) { build(:city, romaji: nil) }
+      it 'is invalid without romaji' do
+        expect(city_3).not_to be_valid
+      end
+    end
+
+    describe 'about location_id' do
+      let(:city_4) { build(:city, location_id: nil) }
+      it 'is invalid without location_id' do
+        expect(city_4).not_to be_valid
+      end
+    end
+  end
 end
