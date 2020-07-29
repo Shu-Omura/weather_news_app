5828fd0145b118ed7d8f47fd874e44fd69e4cf21
Shu Oomura <ohs6261@gmail.com>
2020-07-30 01:53:12 +0900

    Show statistics

--- app/controllers/weather_forecasts_controller.rb
+++ app/controllers/weather_forecasts_controller.rb
@@ -2,5 +2,6 @@ class WeatherForecastsController < ApplicationController
   def index
     @city = City.find(params[:city_id])
     @weather_forecasts = WeatherForecast.where(city: @city)
+    @statistics = Statistic.where(city: @city).recent
   end
 end
--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -2,20 +2,55 @@ class Statistic < ApplicationRecord
   belongs_to :city
   validates :date, presence: true
 
-  def weather_condition
+  scope :recent, -> { order(date: :desc) }
+
+  def weather_condition_d
     case weather_d
     when '快晴'
-      '快晴'
+      '快晴d'
     when /晴/
-      '晴れ'
+      '晴れd'
     when /雪/
-      '雪'
+      '雪d'
     when '大雨'
-      '雨'
+      '雨d'
     when /雨/
-      '小雨'
+      '小雨d'
+    when /曇/
+      '曇りd'
     else
-      '曇り'
+      '霧d'
+    end
+  end
+
+  def weather_condition_n
+    case weather_n
+    when '快晴'
+      '快晴n'
+    when /晴/
+      '晴れn'
+    when /雪/
+      '雪n'
+    when '大雨'
+      '雨n'
+    when /雨/
+      '小雨n'
+    when /曇/
+      '曇りn'
+    else
+      '霧n'
+    end
+  end
+
+  def self.today
+    select do |s|
+      s.date.strftime('%m%d') == Time.current.strftime('%m%d')
+    end
+  end
+
+  def self.tomorrow
+    select do |s|
+      s.date.strftime('%m%d') == Time.current.tomorrow.strftime('%m%d')
     end
   end
 end
